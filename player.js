96ab5153ee432e7d65c5c4ece20d7959cf24cc10
Mahanteshchittapur <101130254+Mahanteshchittapur@users.noreply.github.com>
2023-08-17 12:56:15 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,46 +1,82 @@
-// var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
-var url = "aamp://amssamples.streaming.mediaservices.windows.net/683f7e47-bd83-4427-b0a3-26a6c4547782/BigBuckBunny.ism/manifest(format=mpd-time-csf)";
-// https://https://
-var player;
+ var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+        var player;
 
-window.onload = function() {
-    player = new AAMPMediaPlayer();
-}
+        window.onload = function() {
+            player = new AAMPMediaPlayer();
+        }
 
-function loadAAMPVideo() {
-    var video = document.getElementById("video");
-    video.src = url;
-    video.play();
-    video.style.display = "block";
+        function loadAAMPVideo() {
+            var video = document.getElementById("video");
+            video.src = url;
+            video.play();
+            video.style.display = "block";
+            openFullscreen(video);
 
-    // Request full screen mode
-    if (video.requestFullscreen) {
-        video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) {
-        video.mozRequestFullScreen();
-    } else if (video.webkitRequestFullscreen) {
-        video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) {
-        video.msRequestFullscreen();
-    }
+            // Listen for the 'ended' event of the video to restore hidden elements
+            video.addEventListener('ended', function() {
+                var header = document.querySelector(".header");
+                var carouselContainer = document.getElementById("carouselContainer");
+                var languagesContainer = document.getElementById("languagesContainer");
+                var playbtn = document.querySelector(".play-btn");
 
-    // Hide specified sections
-    var header = document.querySelector(".header");
-    var carouselContainer = document.getElementById("carouselContainer");
-    var languagesContainer = document.getElementById("languagesContainer");
+                header.style.display = "block";
+                carouselContainer.style.display = "block";
+                languagesContainer.style.display = "block";
+                playbtn.style.display = "block";
+            });
+        }
 
-    header.style.display = "none";
-    carouselContainer.style.display = "none";
-    languagesContainer.style.display = "none";
-}
+        var playButton = document.getElementById("playButton");
 
-var playButton = document.getElementById("playButton");
+        playButton.addEventListener("click", loadAAMPVideo);
 
-playButton.addEventListener("keydown", function(event) {
-    if (event.keyCode === 13) { // Enter key
-        loadAAMPVideo();
-    }
-});
+        document.addEventListener("keydown", function(event) {
+            if (event.keyCode === 13) { // Enter key
+                loadAAMPVideo();
+            }
+        });// // var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+// var url = "aamp://amssamples.streaming.mediaservices.windows.net/683f7e47-bd83-4427-b0a3-26a6c4547782/BigBuckBunny.ism/manifest(format=mpd-time-csf)";
+// // https://https://
+// var player;
+
+// window.onload = function() {
+//     player = new AAMPMediaPlayer();
+// }
+
+// function loadAAMPVideo() {
+//     var video = document.getElementById("video");
+//     video.src = url;
+//     video.play();
+//     video.style.display = "block";
+
+//     // Request full screen mode
+//     if (video.requestFullscreen) {
+//         video.requestFullscreen();
+//     } else if (video.mozRequestFullScreen) {
+//         video.mozRequestFullScreen();
+//     } else if (video.webkitRequestFullscreen) {
+//         video.webkitRequestFullscreen();
+//     } else if (video.msRequestFullscreen) {
+//         video.msRequestFullscreen();
+//     }
+
+//     // Hide specified sections
+//     var header = document.querySelector(".header");
+//     var carouselContainer = document.getElementById("carouselContainer");
+//     var languagesContainer = document.getElementById("languagesContainer");
+
+//     header.style.display = "none";
+//     carouselContainer.style.display = "none";
+//     languagesContainer.style.display = "none";
+// }
+
+// var playButton = document.getElementById("playButton");
+
+// playButton.addEventListener("keydown", function(event) {
+//     if (event.keyCode === 13) { // Enter key
+//         loadAAMPVideo();
+//     }
+// });
 
 
 
