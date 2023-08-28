6b265c98fa7e3cfb873d4f7595c2b5e1a1575fcc
Mahanteshchittapur <101130254+Mahanteshchittapur@users.noreply.github.com>
2023-08-28 18:15:18 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,5 +1,4 @@
 
-import { Wifi } from '@firebolt-js/manage-sdk';
 var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
 //var url ="aamps://nlitenams-inso.streaming.media.azure.net/e14ba364-9de4-4252-908d-aa4e622748d7/The_Chase_30_sec_animation.ism/manifest(format=mpd-time-csf,encryption=cenc).mpd";
 //var player;
@@ -33,14 +32,6 @@ function loadAAMPVideo() {
     carouselContainer.style.display = "none";
     languagesContainer.style.display = "none";
     playbtn.style.display = "none";
-
-    // Listen for the 'ended' event to display the homepage sections after video ends
-    // video.addEventListener("ended", function() {
-    //     header.style.display = "block";
-    //     carouselContainer.style.display = "block";
-    //     languagesContainer.style.display = "block";
-    //     playbtn.style.display = "block";
-    // });
 }
 
 var playButton = document.getElementById("playButton");
